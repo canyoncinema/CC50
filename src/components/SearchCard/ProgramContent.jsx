7065fafdf76439e7953383241a889eb2f3be7591
Amy Hua <amy@MacBook-Air-4.local>
2018-06-23 19:04:32 -0700

    Resolve conflicts in ProgramContent.jsx from prior merge

--- src/components/SearchCard/ProgramContent.jsx
+++ src/components/SearchCard/ProgramContent.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
-=======
 import { history } from '../../store';
->>>>>>> feature-add-redux2
 
 import RelatedLinks from '../RelatedLinks/RelatedLinks';
 import RelatedLink from '../RelatedLink/RelatedLink';
@@ -16,19 +13,6 @@ const ProgramContent = ({
 }) => {
 	const listView = viewMode === 'list';
 	return (
-<<<<<<< HEAD
-		<div className="FilmContent">
-			<h6>Curated Program</h6>
-			<h4>
-				<ClampedDescription
-					className="displayName"
-					maxLines={2}
-					title={displayName}>
-					{displayName}
-				</ClampedDescription>
-			</h4>
-			<div className={listView ? 'col-sm-8' : null}>
-=======
 		<div className={listView ? 'row ProgramContent' : 'ProgramContent'}>
 			<div className={listView ? 'col-4' : null}>
 				<h6>Curated Program</h6>
@@ -42,7 +26,6 @@ const ProgramContent = ({
 				</h4>
 			</div>
 			<div className={listView ? filmmakers && filmmakers.length ? 'col-4' : 'col-8' : null}>
->>>>>>> feature-add-redux2
 				<div className={listView ? 'list-center-wrapper' : null}>
 					<ClampedDescription
 						className="description"
@@ -53,11 +36,7 @@ const ProgramContent = ({
 			</div>
 			{
 				filmmakers && filmmakers.length ?
-<<<<<<< HEAD
-				<div className={listView ? 'col-sm-4 filmmakers' : 'filmmakers'}>
-=======
 				<div className={listView ? 'col-4 filmmakers' : 'filmmakers'}>
->>>>>>> feature-add-redux2
 					<div className="no-gutters">
 						<ClampedDescription
 							className="no-gutters"
