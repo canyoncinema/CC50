85409b04e5b382e97933777a7c0ff2abf0f06ba7
Marlo Longley <marlo@codeandcursor.com>
2019-04-02 11:31:55 -0700

    Change order of homepage links

--- src/components/BrowseLinksList/BrowseLinksList.jsx
+++ src/components/BrowseLinksList/BrowseLinksList.jsx
@@ -6,10 +6,10 @@ import BrowseLink from '../BrowseLink/BrowseLink';
 export default () => {
 	return (
 		<div className="BrowseLinksList">
+            <BrowseLink text="Ephemera" search="ephemera" />
+            <BrowseLink text="Curated Programs" search="programs" />
 			<BrowseLink text="Films" search="films" />
 			<BrowseLink text="Filmmakers" search="filmmakers" />
-			<BrowseLink text="Curated Programs" search="programs" />
-			<BrowseLink text="Ephemera" search="ephemera" />
 			<BrowseLink text="Everything" search="" />
     </div>
 	);
