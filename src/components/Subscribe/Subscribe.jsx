c8b259511a3fd2b3f63825b8e8f6eb66a816406a
Marlo Longley <marlo@codeandcursor.com>
2019-04-18 00:03:33 -0700

    Don't overflow in the footer for now

--- src/components/Subscribe/Subscribe.jsx
+++ src/components/Subscribe/Subscribe.jsx
@@ -21,14 +21,14 @@ class Subscribe extends Component {
 					target="_blank"
 					action={`https://canyoncinema.us3.list-manage.com/subscribe/post?u=1a9b415df12c677d769b03069&amp;id=61bff7fcfc&MERGE0=${this.state.email}`}>
 					<Row className="no-gutters">
-						<Col xs={7}>
+						<Col sm={7}>
 							<input type="text"
 								placeholder="Your email"
 								value={this.state.email}
 								onChange={this.onChangeEmail}
 							/>
 						</Col>
-						<Col xs={5}>
+						<Col sm={5}>
 							<input className="ml-auto submit" type="submit" value="SUBSCRIBE" />
 						</Col>
 					</Row>
