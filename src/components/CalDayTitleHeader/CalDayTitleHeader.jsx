19aa4094019b0744c477b879e77691f5f1c95c5c
Marlo Longley <marlo@codeandcursor.com>
2019-04-18 00:01:11 -0700

    Calendar Day responsiveness and truncation

--- src/components/CalDayTitleHeader/CalDayTitleHeader.jsx
+++ src/components/CalDayTitleHeader/CalDayTitleHeader.jsx
@@ -10,20 +10,22 @@ import ClampedDescription from '../ClampedDescription/ClampedDescription';
 const CalDayTitleHeader = ({ startDateTime, tags, author, endDateTime, title, maxLines=2, type }) =>
 	<div className="CalDayTitleHeader container-fluid">
 		<div className="container d-flex">
-			<div className="date-wrapper">
-				{
-					startDateTime && type !== "ephemera" ?
-					<CalDay className="white" dateTime={startDateTime} />
-					: null
-				}
-			</div>
+			{
+				startDateTime && type !== "ephemera" ?
+
+				<div className="date-wrapper">
+                   <CalDay className="white" dateTime={startDateTime}/>
+				</div>
+					:
+					null
+                }
 			<div className="title-wrapper list-center-wrapper">
 				<div>
-					<ClampedDescription maxLines={maxLines}>
+					{/*<ClampedDescription maxLines={maxLines}>*/}
 						<h1 title={title} className="white">
 							{title}
 						</h1>
-					</ClampedDescription>
+					{/*</ClampedDescription>*/}
 					{ author && <div className="author">By {author}</div> }
 					{ tags && <Tags isReadOnly={true} tags={tags} />
 					}
